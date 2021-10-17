0bb75ef9a929618e1db70750a88666ed8bae5d2d
syabyr <mybays@sina.com>
2021-10-18 00:04:15 +0800

    init

--- new_air/newair/src/define.h
+++ new_air/newair/src/define.h
@@ -5,16 +5,16 @@
 
 
 //LED 高电平点亮
-#define LED_PORT GPIOF
-#define LED_PIN GPIO_PIN_4
+#define LED_PORT (GPIOF)
+#define LED_PIN (GPIO_PIN_4)
 
 //key 按下为高电平
-#define KEY_PORT GPIOC
-#define KEY_PIN GPIO_PIN_3
+#define KEY_PORT (GPIOC)
+#define KEY_PIN (GPIO_PIN_3)
 
 //IR 默认为高电平,有信号低电平
-#define IR_PORT GPIOD
-#define IR_PIN GPIO_PIN_2
+#define IR_PORT (GPIOD)
+#define IR_PIN (GPIO_PIN_2)
 
 
 #define MAX_CLEAR_TIMER 1000
--- new_air/newair/src/main.h
+++ new_air/newair/src/main.h
@@ -1,7 +1,47 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+/**
+  ******************************************************************************
+  * @file    EXTI_InterruptPriority\main.h
+  * @author  MCD Application Team
+  * @version V2.0.4
+  * @date    26-April-2018
+  * @brief   This file contains the external declaration for main.c file.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */   
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Evalboard I/Os configuration */
 
 
 
 
-#endif
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Public functions ----------------------------------------------------------*/
+
+#endif /* __MAIN_H */
+
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
